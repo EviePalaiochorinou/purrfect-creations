0abec1fb2611324dc47064e86ea55afe4a143a37
Evie <evangeliapal@gmail.com>
2022-09-28 15:48:06 +0100

    Handlers for total orders and total orders this month

--- client/airtable_api_client.py
+++ client/airtable_api_client.py
@@ -4,45 +4,8 @@ class AirtableApiClient:
 
     base_url = "https://api.airtable.com/v0/app8wLQrrIMrnn673/Orders"
 
-    # Total orders and number of orders in progress
+    # Total orders, number of orders in progress, and total number of orders this month
     def get_orders():
-        return {
-        "records": [
-            {
-                "id": "rec9DWw5D4uSUq1zS",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "order_id": 604,
-                    "order_placed": "2021-10-05",
-                    "product_name": "i heart milk brooch",
-                    "price": 68.83,
-                    "first_name": "Janeva",
-                    "last_name": "Canadine",
-                    "address": "2263 Maple Avenue",
-                    "email": "jcanadinegr@sphinn.com",
-                    "order_status": "in_progress"
-                }
-            },
-            {
-                "id": "rec7956R8a1d3gmVC",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "order_id": 726,
-                    "order_placed": "2021-10-05",
-                    "product_name": "i heart milk brooch",
-                    "price": 181.02,
-                    "first_name": "Lindsay",
-                    "last_name": "Sherrott",
-                    "address": "0 Hovde Junction",
-                    "email": "lsherrottk5@cornell.edu",
-                    "order_status": "placed"
-                }
-            }
-        ]
-    }
-
-    # Get the total number of orders this month
-    def get_orders_this_month():
         return {
         "records": [
             {
--- router/order.py
+++ router/order.py
@@ -1,11 +1,26 @@
 from fastapi import APIRouter
 from client import airtable_api_client
+from datetime import datetime
 
 router = APIRouter(
-    prefix='/order',
-    tags=['order']
+    prefix='/dashboard',
+    tags=['dashboard']
 )
 
-@router.get('/')#, response_model = OrderDisplay)
+@router.get('/orders')#, response_model = OrderDisplay)
 def get_orders():
-    return airtable_api_client.get_orders()
+    orders = airtable_api_client.get_orders()
+    return len(orders)
+
+@router.get('/orders-current-month')
+def get_total_orders_this_month():
+    today = datetime.now()
+    orders = airtable_api_client.get_orders()
+    orders_this_month = []
+    for order in orders:
+        fields = order["fields"]
+        order_date = datetime.fromisoformat(fields["order_placed"])
+        if today.year == order_date.year and today.month == order_date.month:
+            orders_this_month.append(order)
+    return orders_this_month
+
