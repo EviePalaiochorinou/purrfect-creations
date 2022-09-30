88100cc62087cb1e7e374d7d9932eaa66247a006
Evie <evangeliapal@gmail.com>
2022-09-30 13:34:42 +0100

    Refactor Airtable API client to make only one call to the API. Add a limit to number of results return and sort by most recent for ease of development

--- client/airtable_api_client.py
+++ client/airtable_api_client.py
@@ -1,99 +1,14 @@
+from requests import Session
+from pyairtable import Table
 # A class wrapper around Airtable API restful sevice
-
 class AirtableApiClient:
+
+    def __init__(self) -> None:
+        self.airtable_orders_client = Table("keyU8e7I42DvGDdST", "app8wLQrrIMrnn673","tblZBNaHCGVfA9xw1")
 
     base_url = "https://api.airtable.com/v0/app8wLQrrIMrnn673/Orders"
 
-    # Total orders, number of orders in progress, and total number of orders this month
-    def get_orders():
-        return [
-            {
-                "id": "rec9DWw5D4uSUq1zS",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "order_id": 604,
-                    "order_placed": "2021-10-05",
-                    "product_name": "i heart milk brooch",
-                    "price": 68.83,
-                    "first_name": "Janeva",
-                    "last_name": "Canadine",
-                    "address": "2263 Maple Avenue",
-                    "email": "jcanadinegr@sphinn.com",
-                    "order_status": "in_progress"
-                }
-            },
-            {
-                "id": "rec7956R8a1d3gmVC",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "order_id": 726,
-                    "order_placed": "2021-10-05",
-                    "product_name": "i heart milk brooch",
-                    "price": 181.02,
-                    "first_name": "Lindsay",
-                    "last_name": "Sherrott",
-                    "address": "0 Hovde Junction",
-                    "email": "lsherrottk5@cornell.edu",
-                    "order_status": "placed"
-                }
-            }
-        ]
-    # Revenue after removing cancelled orders, keeps only price and order status fields
-    def get_revenue():
-        return {
-        "records": [
-            {
-                "id": "rec9DWw5D4uSUq1zS",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "price": 68.83,
-                    "order_status": "in_progress"
-                }
-            },
-            {
-                "id": "rec7956R8a1d3gmVC",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "price": 181.02,
-                    "order_status": "placed"
-                }
-            }
-        ]
-    }
-
-    # A list of the most recent few orders (use of sort and pageSize)
-    def get_the_10_most_recent_orders():
-        return {
-        "records": [
-            {
-                "id": "rec9DWw5D4uSUq1zS",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "order_id": 604,
-                    "order_placed": "2021-10-05",
-                    "product_name": "i heart milk brooch",
-                    "price": 68.83,
-                    "first_name": "Janeva",
-                    "last_name": "Canadine",
-                    "address": "2263 Maple Avenue",
-                    "email": "jcanadinegr@sphinn.com",
-                    "order_status": "in_progress"
-                }
-            },
-            {
-                "id": "rec7956R8a1d3gmVC",
-                "createdTime": "2021-10-06T09:16:38.000Z",
-                "fields": {
-                    "order_id": 726,
-                    "order_placed": "2021-10-05",
-                    "product_name": "i heart milk brooch",
-                    "price": 181.02,
-                    "first_name": "Lindsay",
-                    "last_name": "Sherrott",
-                    "address": "0 Hovde Junction",
-                    "email": "lsherrottk5@cornell.edu",
-                    "order_status": "placed"
-                }
-            }
-        ]
-    }
+    # I have limited the number of the results coming back, for ease of development and a faster response
+    def get_orders(self):
+        orders = self.airtable_orders_client.all(sort=["-order_placed"], max_records = 50)
+        return orders
--- router/order.py
+++ router/order.py
@@ -9,7 +9,7 @@ router = APIRouter(
 
 @router.get('/')
 def get_aggregate_data():
-    client = airtable_api_client.AirtableApiClient
+    client = airtable_api_client.AirtableApiClient()
     orders = client.get_orders()
     aggregate_data = {
         "number of orders": _get_orders(orders),
@@ -25,14 +25,15 @@ def _get_orders(orders):
     return len(orders)
 
 def _get_total_orders_this_month(orders):
-    today = datetime.now()
-    orders_this_month = []
+    # Most recent order is quite old so we mock the current date to coincide with the last order's date for the sake of getting data back.
+    today = _get_now()
+    total_orders_this_month = 0
     for order in orders:
         fields = order["fields"]
         order_date = datetime.fromisoformat(fields["order_placed"])
         if today.year == order_date.year and today.month == order_date.month:
-            orders_this_month.append(order)
-    return orders_this_month
+            total_orders_this_month += 1
+    return total_orders_this_month
 
 def _get_orders_in_progress(orders):
     orders_in_progress = []
@@ -49,6 +50,7 @@ def _get_revenue(orders):
         fields = order["fields"]
         if fields["order_status"] != "cancelled":
             revenue += fields["price"]
+    # Ideally a float with two decimal places
     return revenue
 
 def _get_recent_orders(orders):
@@ -62,3 +64,6 @@ def _get_placed_orders(orders):
         if fields["order_status"] == "placed":
             placed_orders.append(order)
     return placed_orders
+
+def _get_now():
+    return datetime.fromisoformat("2021-10-10")
